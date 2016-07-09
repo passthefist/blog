775b1a35c6be048bc664f3085f07eb22477c57f0
rgoetz <rob@betterworldcollective.com>
2016-07-09 21:14:19 +0100

    [blog] Add files for testing basic enginge

--- blog/js/engine.js
+++ blog/js/engine.js
@@ -1,47 +1,74 @@
 (function() {
-    this.Http = function() {
-        this.get = function(url, onCompleteCb) {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function()
-            {
-                if (xhr.readyState == 4 && xhr.status == 200)
-                {
-                    onCompleteCb(JSON.parse(xhr.responseText));
-                }
-            };
-
-            xhr.open("GET", url);
-            xhr.send();
-        }
-    }
-}).apply(window);
-
-(function() {
     this.Blog = function() {
         var key = '8086d62266ceb885af4862ed5ff1387f5293c36d';
-        var http = new Http();
 
         var api = function(endpoint) {
-            return 'https://rdgoetz:'+key+'@/'+endpoint;
+            return 'https://rdgoetz:'+key+'@api.github.com/'+endpoint;
         }
 
+        var templates = {};
+
+        $.ajax('https://raw.githubusercontent.com/rdgoetz/blog/gh-pages/blog/templates/post.html', {
+            crossDomain: true,
+            mimeType:'text/plain'
+        }).done(function(response) {
+            templates.post = response;
+        }).fail(function(){
+            console.log(arguments)
+        });
+
         this.posts = function(year,callback) {
-            http.get(api('repos/rdgoetz/blog/contents/blog/posts/'+year),function(response) {
+            $.ajax(api('repos/rdgoetz/blog/contents/blog/posts/'+year), {
+                dataType: 'jsonp',
+                jsonp:'callback',
+                crossDomain: true
+            }).done(function(response) {
                 var posts = [];
 
-                for(var i in response) {
-                    var post = response[i];
+                for(var i in response.data) {
+                    var post = response.data[i];
                     var name = post.name;
-                    var meta = post.name.split('|');
+                    var meta = post.name.split('-');
+
+                    var dates = meta[0].split(',')
+                    var day = dates[0];
+                    var month = dates[1];
+                    var title = meta[1];
+
+                    var timestamp = new Date(year,month,day);
 
                     posts.push({
-                        file: name,
-                        date: meta[0],
-                        title: meta[1]
+                        file: year+'/'+name,
+                        date: timestamp.getTime(),
+                        title: title
                     });
                 }
 
                 callback(posts);
+            }).fail(function(){
+                console.log(arguments)
+            });
+        }
+
+        this.postContent = function(post,callback) {
+            $.ajax('https://raw.githubusercontent.com/rdgoetz/blog/gh-pages/blog/posts/'+post.file, {
+                crossDomain: true,
+                mimeType:'text/plain'
+            }).done(function(response) {
+                callback(marked(response));
+            }).fail(function(){
+                console.log(arguments)
+            });
+        }
+
+        this.renderPostInto = function(post, $el) {
+            this.postContent(post, function(content) {
+                $template = $(templates.post);
+                $template.find('[data-post-title]').html(post.title);
+                $template.find('[data-post-date]').html(post.date);
+                $template.find('[data-post-content]').html(content);
+
+                $el.append($template);
             });
         }
     }
